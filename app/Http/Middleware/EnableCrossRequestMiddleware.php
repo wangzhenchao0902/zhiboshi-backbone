f0ca56ee3c9a07f7602be21a8238586d10aa038d
苏向东 <suxiangdong@zcool.com.cn>
2021-03-06 13:08:58 +0800

    修改中间件逻辑

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class EnableCrossRequestMiddleware
 {
@@ -16,7 +17,11 @@ class EnableCrossRequestMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
+        $response = $next($request);
+        if ($response instanceof BinaryFileResponse) {
+            return $response;
+        }
+        return $response->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization,     X-Requested-With, Application');
     }
