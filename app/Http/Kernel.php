3c34198b317efd4b93af65f22eca7fa36c68781d
suxiangdong <suxiangdong@baidu.com>
2021-05-16 20:29:17 +0800

    后端替换session_name「

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,8 @@ class Kernel extends HttpKernel
         'manager' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\StartSession::class,
+            \App\Http\Middleware\ManagerSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             // \App\Http\Middleware\VerifyCsrfToken::class,
@@ -51,7 +52,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:api',
+            // 'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
---
+++ app/Http/Middleware/ManagerSession.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Session\Middleware\StartSession;
+use Closure;
+use Illuminate\Http\Request;
+
+class ManagerSession extends StartSession
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (! $this->sessionConfigured()) {
+            return $next($request);
+        }
+
+        $session = $this->getSession($request);
+
+        if ($this->manager->shouldBlock() ||
+            ($request->route() instanceof Route && $request->route()->locksFor())) {
+            return $this->handleRequestWhileBlocking($request, $session, $next);
+        } else {
+            return $this->handleStatefulRequest($request, $session, $next);
+        }
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function getSession(Request $request)
+    {
+        return tap($this->manager->driver(), function ($session) use ($request) {
+            $session->setId($request->cookies->get('zbs_session'));
+        });
+    }
+}
