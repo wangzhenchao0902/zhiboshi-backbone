5924d4333536dabf0e23cb74609c33b789c79d8b
wangzhenchao <693892038@qq.com>
2022-01-07 01:56:32 +0800

    anli update

--- app/Http/Controllers/Manager/ArticleController.php
+++ app/Http/Controllers/Manager/ArticleController.php
@@ -77,7 +77,7 @@ class ArticleController extends Controller
      */
     public function index(Request $request)
     {
-        $conditions = array_merge(array_filter($request->except('per_page', 'page')), ['category_id', "<>", 2]);
+        $conditions = array_merge(array_filter($request->except('per_page', 'page')), ['category_id' => "in [1,3,4]"]);
         $articles = $this->articleRep->paginate($request->input('per_page', 20), $conditions);
         $articles->getCollection()->transform(function($item)
         {
