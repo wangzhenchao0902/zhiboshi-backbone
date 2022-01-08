a3deb68a00649c3317e6345c67809e54865f4ceb
wangzhenchao <693892038@qq.com>
2022-01-08 17:57:35 +0800

    anli update

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -30,8 +30,7 @@ class ArticleController extends Controller
     {
         $tags = $request->input('brand', '');
         if($tags !== '') {
-            return success($tags);
-            $conditions = array_merge(array_filter($request->except('per_page', 'page')), ['category_id' => 2, 'status' => 1, 'tags' => $tags]);
+            $conditions = array_merge(array_filter($request->except('per_page', 'page')), ['category_id' => 2, 'status' => 1, 'tags' => urlencode($tags)]);
         } else {
             $conditions = array_merge(array_filter($request->except('per_page', 'page')), ['category_id' => 2, 'status' => 1]);
         }
